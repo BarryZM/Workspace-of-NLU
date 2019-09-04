70bc17e43e6164c7713a54df236e99308b197cd9
Apollo2Mars <Apollo2Mars@gmail.com>
2019-09-04 13:37:07 +0800

    update

--- lexical_analysis/models/BIRNN_CRF.py
+++ lexical_analysis/models/BIRNN_CRF.py
@@ -1,3 +1,5 @@
+# problem : tokenizer 变长 encode
+
 import tensorflow as tf
 
 
@@ -49,7 +51,7 @@ class BIRNN_CRF(object):
                         cell_fw=cell_fw,
                         cell_bw=cell_bw,
                         inputs=inputs_emb,
-                        sequence_length=tf.tile(self.seq_len, self.args.batch_size),
+                        sequence_length=tf.tile(self.seq_len, [self.args.batch_size]),
                         dtype=tf.float32)
                     outputs = tf.concat([output_fw_seq, output_bw_seq], axis=-1)
                     outputs = tf.nn.dropout(outputs, self.dropout_pl)
