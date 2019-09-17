d350eee692b57d8bba1432a66a27210119b2213d
Apollo2Mars <Apollo2Mars@gmail.com>
2019-09-17 15:14:42 +0800

    update

--- utils/Dataset_ABSA_TF.py
+++ utils/Dataset_ABSA_TF.py
@@ -6,49 +6,24 @@
 # @Contact : apollo2mars@gmail.com
 # @Desc    :
 
+import numpy as np
+
 
 class Dataset_ABSA():
-     def __init__(self, fname, tokenizer, label_str):
-         self.label_str = label_str
-         self.label_list = self.set_label_list()
-         self.aspect2id = self.set_aspect2id()
-         self.aspect2onehot = self.set_aspect2onehot()
+     def __init__(self, corpus, tokenizer, max_seq_len, data_type, tag_list):
+         self.corpus = corpus
+         self.label_list = tag_list
+         self.tokenizer = tokenizer
+         self.max_seq_len = max_seq_len
+         self.data_type = data_type
+
+         self.tag2id = self.set_tag_dict()
+         self.tag2onehot = self.set_tag2onehot()
+         self.preprocess()
 
          print(self.label_list)
          print(self.aspect2id)
          print(self.aspect2onehot)
-
-         fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-         lines = fin.readlines()
-         fin.close()
-
-         text_list = []
-         term_list = []
-         aspect_list = []
-         aspect_onehot_list=[]
-         data_list = []
-
-         for i in range(0, len(lines), 4):
-             text  = lines[i].lower().strip()
-             term  = lines[i+1].lower().strip()
-             aspect = lines[i + 2].lower().strip()
-             polarity = lines[i + 3].strip()
-
-             assert polarity in ['-1', '0', '1'], print("polarity", polarity)
-             text_idx = tokenizer.text_to_sequence(text)
-             term_idx = tokenizer.text_to_sequence(term)
-             aspect_idx = self.aspect2id[aspect]
-             aspect_onehot_idx = self.aspect2onehot[aspect]
-
-             text_list.append(text_idx)
-             term_list.append(term_idx)
-             aspect_list.append(aspect_idx)
-             aspect_onehot_list.append(aspect_onehot_idx)
-
-         self.text_list = np.asarray(text_list)
-         self.term_list = np.asarray(term_list)
-         self.aspect_list = np.asarray(aspect_list)
-         self.aspect_onehot_list = np.asarray(aspect_onehot_list)
 
      def __getitem__(self, index):
          return self.text_list[index]
@@ -77,3 +52,56 @@ class Dataset_ABSA():
          for aspect, vector in zip(label_list, one_hot_df):
              label_dict[aspect] = vector
          return label_dict
+
+     def encode_text_sequence(self, text, do_padding, do_reverse):
+         """
+         :param text:
+         :return: convert text to numberical digital features with max length, paddding
+         and truncating
+         """
+         words = list(text)
+
+         sequence = [self.word2idx[w] if w in self.word2idx else
+                     self.word2idx['<UNK>'] for w in words]
+
+         if len(sequence) == 0:
+             sequence = [0]
+         if do_reverse:
+             sequence = sequence[::-1]
+
+         if do_padding:
+             sequence = self.__pad_and_truncate(sequence, self.max_seq_len, value=0)
+
+         return sequence
+
+     def preprocess(self):
+         fin = open(self.corpus, 'r', encoding='utf-8', newline='\n', errors='ignore')
+         lines = fin.readlines()
+         fin.close()
+
+         text_list = []
+         term_list = []
+         aspect_list = []
+         aspect_onehot_list = []
+
+         for i in range(0, len(lines), 4):
+             text = lines[i].lower().strip()
+             term = lines[i + 1].lower().strip()
+             aspect = lines[i + 2].lower().strip()
+             polarity = lines[i + 3].strip()
+
+             assert polarity in ['-1', '0', '1'], print("polarity", polarity)
+             text_idx = self.encode_text_sequence(text)
+             term_idx = self.encode_text_sequence(term)
+             aspect_idx = self.tag2id[aspect]
+             aspect_onehot_idx = self.tag2onehot[aspect]
+
+             text_list.append(text_idx)
+             term_list.append(term_idx)
+             aspect_list.append(aspect_idx)
+             aspect_onehot_list.append(aspect_onehot_idx)
+
+         self.text_list = np.asarray(text_list)
+         self.term_list = np.asarray(term_list)
+         self.aspect_list = np.asarray(aspect_list)
+         self.aspect_onehot_list = np.asarray(aspect_onehot_list)
