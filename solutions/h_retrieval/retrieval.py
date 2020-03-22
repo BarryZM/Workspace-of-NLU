785e0b1f84604a1babe0fa0eb005977c979f6bf6
Apollo2Mars <Apollo2Mars@gmail.com>
2020-03-22 23:11:35 +0800

    update retreval.py

--- solutions/h_retrieval/retrieval.py
+++ solutions/h_retrieval/retrieval.py
@@ -13,12 +13,12 @@ es_pwd = ''
 es_port = ''
 es_document_size = 5
 es_index_name = 'jd_index_name' # ES 6.3
-# es = Elasticsearch([es_ip],http_auth=(es_user, es_pwd), port=es_port)
+es = Elasticsearch([es_ip],http_auth=(es_user, es_pwd), port=es_port)
 " bert-as-service init"
-# bc_embedding_path = '../../resources/clue-pair'
-# if os.path.exists(bc_embedding_path) is False:
-#     raise RuntimeError('embedding file is not exist')
-# os.system('bert-serving-start -model_dir ' + bc_embedding_path + ' -num_worker=4 ')
+bc_embedding_path = '../../resources/clue-pair'
+if os.path.exists(bc_embedding_path) is False:
+     raise RuntimeError('embedding file is not exist')
+os.system('bert-serving-start -model_dir ' + bc_embedding_path + ' -num_worker=4 ')
 bc_embedding_size = 312
 bc_document_size = 5
 bc = BertClient()
@@ -34,7 +34,7 @@ def elastic_search_by_qq(input_text):
     use in dialogue system
     use elastic search to make query-question pair retrieval 
     """
-    doc = {"question": {"bool": {'should':[{'match':{'query':input_text}}]}}}
+    doc = {"question": {"bool": {'should':[{'match':{'question':input_text}}]}}}
     searched = es.search(index=es_index_name, body=doc, size=es_document_size)
     document_list = []
     for hit in searched['hits']['hits']:
@@ -46,7 +46,8 @@ def elastic_search_by_qa(input_text):
     use in dialogue system
     use elastic search to make query-answer pair retrieval 
     """
-    doc = {"question":{'match_parse':{'documents':input_text}}}
+    # doc = {"question":{'match_parse':{'documents':input_text}}} # 精确匹配
+    doc = {"question":{'match':{'documents':input_text}}} # 精确匹配
     searched = es.search(index=es_index_name, body=doc, size=es_document_size)
     document_list = []
     for hit in searched['hits']['hits']:
